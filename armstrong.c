429b1103336be6b45f18094327a3d1cec012aa7d
Vibha Mandal <vibhamandal05@gmail.com>
2026-07-17 20:25:47 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num, temp, rem, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while (temp != 0) {
+        rem = temp % 10;
+        sum = sum + (rem * rem * rem);
+        temp = temp / 10;
+    }
+
+    if (sum == num) {
+        printf("%d is an Armstrong number.", num);
+    } else {
+        printf("%d is not an Armstrong number.", num);
+    }
+
+    return 0;
+}
